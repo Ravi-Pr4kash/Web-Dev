b8300722495b383ebbbfea7072d72af184bfd56d
Ravi-Pr4kash <raviprakash0818@gmail.com>
2024-12-05 21:23:41 +0530

    zod

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,23 +1,63 @@
+const bcrypt = require('bcrypt'); 
 const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken')
 const JWT_SECRET = "RAVI@PRAKASH"
 const {UserModel,TodoModel} = require("./db");
 const { mongo, default: mongoose } = require('mongoose');
+const { z }  = require('zod')
+
+
+
 mongoose.connect("mongodb+srv://admin:admin@cluster0.ly7z4.mongodb.net/ravi-todo-0")
 
 app.use(express.json()); 
 
 app.post('/signup', async(req,res)=>{
+    const requiredBody = z.object({
+        email: z.string().min(3).max(100).email(),
+        name: z.string().min(4).max(100),
+        password: z.string().min(6).max(25) 
+    })
+
+    // const parsedData = requiredBody.parse(req.body);
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if (!parsedDataWithSuccess.success) {
+        res.json({
+            message: "incorrect format",
+            eroor: parsedDataWithSuccess.error 
+        })
+        return
+    }
+
+
+
      const email = req.body.email
      const password = req.body.password
      const name = req.body.name
 
+     try {
+        const hashedPassword = await bcrypt.hash(password, 5); 
+     console.log(hashedPassword )
+
      await UserModel.create({
         email: email,
-        password: password,
-        nmae: name
+        password: hashedPassword ,
+        name: name
      });
+     } catch (e) {
+        res.json({
+            message: "User already exits"
+        })
+        errorThrown = true;
+     }
+
+     if(!errorThrown){
+        res.json({
+            message: "You are signed in"
+        })
+     }
 
      res.json("You are signed up")
 })
@@ -28,10 +68,17 @@ app.post('/signin', async (req,res)=>{
 
     const user = await UserModel.findOne({
         email: email,
-        password: password
-    })
+    });
+
+    if(!user){
+        res.status(403).json({
+            message: "user does not exist in our database"
+        })
+    }
+
+    const passwordMatched = await bcrypt.compare(password, user.password);
  
-    if(user){
+    if(passwordMatched){
         const token = jwt.sign({
             id: user._id.toString()
         },JWT_SECRET);
